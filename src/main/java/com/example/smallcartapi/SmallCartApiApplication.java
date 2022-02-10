0548df59aeb0b850ac74011784fb0e9f22cee88e
olowo-coder <olowofela.olanrewaju@decagon.dev>
2022-02-10 19:21:55 +0100

    first

---
+++ src/main/java/com/example/smallcartapi/SmallCartApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.smallcartapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallCartApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmallCartApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/smallcartapi/SmallCartApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smallcartapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallCartApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
